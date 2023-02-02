9024aab69b1f312b5fe579ee65ed60c49f1ad25c
Amaya <jonathan.amaya@softwareone.com>
2023-02-02 16:41:36 -0500

    First commit

---
+++ Controllers/TodosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace my_todos_backend.Controllers
+{
+    [Route("api/[todos]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace my_todos_backend.Models
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string startDate { get; set; }
+        public string dueDate { get; set; }
+        public string description { get; set; }
+    }
+}
